061d4d8cc955ad6779936119d8c524fc972113ca
Paramesh <parameshdupam9652@gmail.com>
2024-08-29 20:59:07 +0530

    first commit

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -58,7 +58,7 @@ class Repository extends Component {
 
     this.setState({apiStatus: apiStatusConstants.inProgress})
 
-    const url = `` // access_token
+    const url = `https://apis2.ccbp.in/gpv/repos/${username}?api_key=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` // access_token
     const options = {
       method: 'GET',
     }
