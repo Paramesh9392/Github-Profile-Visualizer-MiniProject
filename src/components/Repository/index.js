d9a23d827a8d2d0c639832798caac9b712d9ece8
Paramesh <parameshdupam9652@gmail.com>
2024-09-01 21:16:38 +0530

    first commit

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -58,7 +58,7 @@ class Repository extends Component {
 
     this.setState({apiStatus: apiStatusConstants.inProgress})
 
-    const url = `https://apis2.ccbp.in/gpv/repos/${username}?api_key=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` // access_token
+    const url = `https://apis2.ccbp.in/gpv/repos/${username}?api_key=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` // access_token
     const options = {
       method: 'GET',
     }
@@ -214,7 +214,7 @@ class Repository extends Component {
   )
 
   renderLoaderView = () => (
-    <div className="repository-loader-container">
+    <div className="repository-loader-container" testid="loader">
       <Loader type="TailSpin" color="#3B82F6" height={50} width={50} />
     </div>
   )
